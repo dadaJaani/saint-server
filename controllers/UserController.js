a376f3ca76f7962c452ab367a6e318a65eae1011
Waqas Rehmani <dada.scene@gmail.com>
2019-04-13 16:26:16 +1000

    init 2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,3 +2,151 @@ var mongoose = require('mongoose');
 
 // Import user schema
 var Users = mongoose.model('users');
+
+// =============================================
+// USER FUNCTIONS
+// =============================================
+// Helper function to be used in other functions
+// var assembleUser = function (req) {
+//     var user = {
+//         "id":req.body.username, // WE GETTING USERNAME BECAUSE FRONTEND SENDS USERNAME
+//         "password":req.body.password,
+//         "email":req.body.email,
+//         "fname":req.body.fname,
+//         "lname":req.body.lname,
+//         "points":req.body.points,
+//         "photo":req.body.photo,
+//     };
+//     return user;
+// };
+
+// FUNCTION TO CREATE USER
+var createUser = function(req, res){
+    var newUser = new Users(
+        {
+            "id":req.body.username, // WE GETTING USERNAME BECAUSE FRONTEND SENDS USERNAME
+            "password":req.body.password,
+            "email":req.body.email,
+            "fname":req.body.fname,
+            "lname":req.body.lname,
+            "photo": '',
+            "dob": '',
+            "medicalHistory": [],
+            "medicalId": '',
+            "insuranceId": '',
+        }
+    );
+
+    newUser.save(function (err, createdUser){
+        if (!err){
+            res.send(createdUser);
+        }
+        else {
+            console.log(err)
+            res.send(err);
+        }
+    });
+};
+
+// FUNCTION TO EDIT USER
+var editUser = function(req, res){
+    var userId = req.params.id;
+    var userUpdate = {
+        "password":req.body.password,
+        "email":req.body.email,
+        "fname":req.body.fname,
+        "lname":req.body.lname,
+        "points":req.body.points,
+        "photo":req.body.photo,
+        "dob": '',
+        "medicalHistory": [],
+        "medicalId": '',
+        "insuranceId": '',
+    };
+
+    Users.updateOne({ id: userId }, userUpdate, function(err, updatedUser) {
+        if (!err){
+            res.send(updatedUser);
+        }
+        else {
+            res.send(err);
+        }
+    });
+};
+
+// FUNCTION TO GET ALL USER
+var findAllUsers = function(req,res){
+
+    Users.find(function(err, allUsers){
+        if(!err){
+            res.send(allUsers);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+};
+
+// FUNCTION TO SEARCH A USER
+var getUserData = function(req, res){
+    var userId = req.params.id;
+    Users.find({id:userId},function(err,searchedUser){
+        if(!err){
+            res.send(searchedUser);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+};
+
+// FUNCTION TO DELETE USER
+var deleteUser = function(req, res){
+    var userId = req.params.id;
+
+    Users.deleteOne({ id: userId } ,function(err){
+        if(!err){
+            res.send(userId);
+        }else{
+            res.sendStatus(404);
+        }
+    });
+
+};
+
+// FUNCTION TO GET CHECK IF USER ID EXISTS
+var validateUserID = function (req, res){
+    var userID = req.params.id;
+
+    User.findOne({ id: userID }, function(err, theUser){
+        if (theUser)
+            res.send(true)
+        else
+            res.send(false)
+    });
+};
+
+// FUNCTION TO LOGIN WHEN USER SIGNS IN
+var loginUser = function (req, res){
+    var userID = req.params.id;
+
+    var loginAttempt = {
+        "id":       req.body.username,
+        "password": req.body.password,
+    };
+
+    User.findOne({ id: loginAttempt.id, password: loginAttempt.password }, function(err, theUser){
+        if (theUser)
+            res.send(theUser)
+        else
+            res.send(false)
+    });
+};
+
+
+module.exports.createUser         = createUser;
+module.exports.editUser           = editUser;
+module.exports.findAllUsers       = findAllUsers;
+module.exports.getUserData        = getUserData;
+module.exports.deleteUser         = deleteUser;
+module.exports.validateUserID     = validateUserID;
+module.exports.loginUser          = loginUser;
+module.exports.getRewardsForUsers = getRewardsForUsers;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var UserController = require('../controllers/UserController.js');
+var restaurant_controller = require('../controllers/restaurant_controller.js');
+var reward_controller = require('../controllers/reward_controller.js');
+var sale_controller = require('../controllers/sale_controller.js');
+
 // Get the index page
 router.get('/', function(req, res, next) {
   res.send("Saint server is running");
 });
+
+// ========================================================================
+// Routes for USER FUNCTIONALITY
+// ========================================================================
+// Create user
+router.post('/user', UserController.createUser);
+// Edit user
+router.put('/user/:id', UserController.editUser);
+// Get all users
+router.get('/user/all',UserController.findAllUsers);
+// Validate a user ID
+router.get('/user/checkID/:id', UserController.validateUserID);
+// Login and send user if ID and password is correct
+router.post('/user/login/:id', UserController.loginUser);
+// get a user data
+router.get('/user/get/:id',UserController.getUserData);
+// Delete a user
+router.delete('/user/:id', user_controller.deleteUser);
 
 
 
